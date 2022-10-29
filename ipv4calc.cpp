19992fc6a1805ab18fd85ed80a3bb02b0ae98aa5
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-10-29 19:42:32 +0700

    change style

--- ipv4calc.cpp
+++ ipv4calc.cpp
@@ -2,6 +2,7 @@
 #include "ipv4calc.h"
 #include <glibmm/ustring.h>
 #include <gtkmm/box.h>
+#include <gtkmm/entry.h>
 #include <gtkmm/enums.h>
 #include <gtkmm/widget.h>
 #include <gtkmm/window.h>
@@ -11,39 +12,59 @@ ipv4calc::ipv4calc ()
 {
         const Glib::ustring app_title = "ipv4 calculator";
         Gtk::Window::set_title (app_title);
-        Gtk::Widget::set_size_request (800, 500);
+        Gtk::Widget::set_size_request (900, 500);
         this->v_gtkmm_box = (Gtk::Box)Gtk::Orientation::VERTICAL;
         set_child (this->v_gtkmm_box);
 
-        this->make_entry ();
-        this->entry_and_submit.append (this->gtkmm_entry);
+        this->make_ip_entry ();
+        this->entry_and_submit.append (this->entry_ip);
 
         this->make_button ();
         this->entry_and_submit.append (this->gtkmm_button);
         v_gtkmm_box.append (this->entry_and_submit);
-        this->gtkmm_button.signal_clicked().connect(sigc::mem_fun(*this, &ipv4calc::on_submit));
+        this->gtkmm_button.signal_clicked ().connect (
+            sigc::mem_fun (*this, &ipv4calc::on_submit));
 }
 
 void
-ipv4calc::on_submit()
+ipv4calc::on_submit ()
 {
-        printf("%s\n", this->gtkmm_entry.get_text().c_str());
+        printf ("%s\n", this->gtkmm_entry.get_text ().c_str ());
 }
 
 void
-ipv4calc::make_entry ()
+ipv4calc::make_ip_entry ()
 {
-        this->gtkmm_entry.set_max_length (18);
-        this->gtkmm_entry.set_text ("255.255.255.0/24");
-        this->gtkmm_entry.set_margin (10);
+        int max_byte_length = 3;
+        int max_cidr_length = 2;
+
+        this->ip_byte1.set_margin (10);
+        this->ip_byte1.set_max_length (max_byte_length);
+
+        this->ip_byte2.set_margin (10);
+        this->ip_byte2.set_max_length (max_byte_length);
+
+        this->ip_byte3.set_margin (10);
+        this->ip_byte3.set_max_length (max_byte_length);
+
+        this->ip_byte4.set_margin (10);
+        this->ip_byte4.set_max_length (max_byte_length);
+
+        this->ip_cidr.set_margin (10);
+        this->ip_cidr.set_max_length (max_cidr_length);
+
+        this->entry_ip.append (this->ip_byte1);
+        this->entry_ip.append (this->ip_byte2);
+        this->entry_ip.append (this->ip_byte3);
+        this->entry_ip.append (this->ip_byte4);
+        this->entry_ip.append (this->ip_cidr);
 }
 
 void
 ipv4calc::make_button ()
 {
-        this->gtkmm_button = (Gtk::Button) "btn";
+        this->gtkmm_button = (Gtk::Button) "calculate!";
         this->gtkmm_button.set_margin (10);
-        
 }
 
 void
--- ipv4calc.h
+++ ipv4calc.h
@@ -1,6 +1,7 @@
 #ifndef IPV4_CALC_H
 #define IPV4_CALC_H
 
+#include <glibmm/ustring.h>
 #include <gtkmm.h>
 #include <gtkmm/button.h>
 #include <gtkmm/window.h>
@@ -17,8 +18,17 @@ class ipv4calc : public Gtk::Window
         Gtk::Entry gtkmm_entry;
 
         Gtk::Button gtkmm_button;
+        Glib::ustring ip_and_cidr;
 
-        void make_entry ();
+        // for 4 byte of ip and cidr value 
+        Gtk::Entry ip_byte1;
+        Gtk::Entry ip_byte2;
+        Gtk::Entry ip_byte3;
+        Gtk::Entry ip_byte4;
+        Gtk::Entry ip_cidr;
+        Gtk::Box   entry_ip;
+
+        void make_ip_entry ();
         void make_button ();
         void on_button_clicked ();
         void on_submit();
