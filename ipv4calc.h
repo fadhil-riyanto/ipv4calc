6160786c58f7bd72037f2bb2cb6f18d8e0a75cbf
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-10-29 18:04:22 +0700

    add onclicked

--- ipv4calc.h
+++ ipv4calc.h
@@ -13,10 +13,15 @@ class ipv4calc : public Gtk::Window
 
       private:
         Gtk::Box v_gtkmm_box;
-        Gtk::Box h_gtkmm_box;
+        Gtk::Box entry_and_submit;
         Gtk::Entry gtkmm_entry;
 
-        void make_entry(Gtk::Entry* gtk_entry);
+        Gtk::Button gtkmm_button;
+
+        void make_entry ();
+        void make_button ();
+        void on_button_clicked ();
+        void on_submit();
 };
 
 #endif
