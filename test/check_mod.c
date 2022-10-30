317d2d02c2d4d5875ed0207b497ccd12c69aee05
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-10-30 13:26:22 +0700

    add reverse order & make it to 8 bit format

--- test/check_mod.c
+++ test/check_mod.c
@@ -3,21 +3,25 @@
 int
 main (int argv, char *argc[])
 {
-        int data = 8;
+        int data = 74;
         int changed_dataf = data;
-        int binnums;
-
+        int binnums[8];
+        int i = 0;
         for (; changed_dataf > 0;)
                 {
-
-                        // binnums = changed_dataf % (float)2;
-                        binnums = changed_dataf % 2;
+                        binnums[i++] = changed_dataf % 2;
                         changed_dataf = changed_dataf / 2;
-                        
-                        fprintf (stdout, "%d", binnums);
                 }
-       
+        
+        // fill last byte with zero
+        for (; i < 8;)
+                {
+                        binnums[i++] = 0;
+                }
+        for (int a = 8; a > 0; a--)
+                {
+                        fprintf (stdout, "%d", binnums[a - 1]);
+                }
         // changed_dataf = changed_dataf / 2;
         // binnums = changed_dataf % 2;
-        
 }
